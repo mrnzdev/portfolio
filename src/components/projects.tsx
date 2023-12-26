397408868cfc5900413bcdde0b0088cd462049a0
mrnzdev <contact@mrnz.dev>
2023-12-25 23:23:27 -0300

    v0.13

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -10,31 +10,14 @@ export default function Projects() {
 						href='https://mrnz.dev'>
 						<div>
 							<span className='font-medium underline underline-offset-4 '>
-								Project 1
+								MinimalGmail
 							</span>
 							<span className='ml-2 rounded-lg px-2 py-0.5 font-mono text-xs no-underline bg-primary text-primary-foreground'>
 								In development
 							</span>
 						</div>
 						<span className='text-muted-foreground'>
-							Project 1 description
-						</span>
-					</a>
-				</li>
-				<li>
-					<a
-						className='flex flex-col space-y-1.5'
-						href='https://mrnz.dev'>
-						<div>
-							<span className='font-medium underline underline-offset-4 '>
-								Project 2
-							</span>
-							<span className='ml-2 rounded-lg px-2 py-0.5 font-mono text-xs no-underline bg-primary text-primary-foreground'>
-								In development
-							</span>
-						</div>
-						<span className='text-muted-foreground'>
-							Project 2 description
+							Email service that allows you to use gmail, but with a minimal UI.
 						</span>
 					</a>
 				</li>
