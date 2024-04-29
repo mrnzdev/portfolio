90258a5c3037a9d034e0bf2e413f6774c41ea011
mrnzdev <contact@mrnz.dev>
2024-04-29 08:36:00 -0300

    Refactor Projects component to use ProjectItem component

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,40 +1,23 @@
-export default function Projects() {
+import ProjectItem from "@/components/project-item";
+
+const Projects = () => {
 	return (
 		<main className="container">
 			<ul className="space-y-4 py-4">
-				<li>
-					<div className="flex flex-col space-y-1.5">
-						<div>
-							<span className="font-medium underline underline-offset-4 ">
-								<a href="https://neatmail.xyz">Neat Mail</a>
-							</span>
-							<span className="ml-2 rounded-lg px-2 py-0.5 font-mono text-xs no-underline bg-primary text-primary-foreground">
-								In development
-							</span>
-						</div>
-						<span className="text-muted-foreground">
-							Gmail service with a simple and minimalist approach.
-						</span>
-					</div>
-				</li>
-				<li>
-					<div className="flex flex-col space-y-1.5">
-						<div>
-							<span className="font-medium underline underline-offset-4 ">
-								<a href="https://marketplace.visualstudio.com/items?itemName=mrnzdev.neat-theme">
-									Neat Theme
-								</a>
-							</span>
-							<span className="ml-2 rounded-lg px-2 py-0.5 font-mono text-xs no-underline bg-primary text-primary-foreground">
-								v0.1.2
-							</span>
-						</div>
-						<span className="text-muted-foreground">
-							Sleek black VSCode color theme.
-						</span>
-					</div>
-				</li>
+				<ProjectItem
+					url="https://neatmail.xyz"
+					title="Neat Mail"
+					status="In development"
+					description="Gmail service with a simple and minimalist approach."
+				/>
+				<ProjectItem
+					url="https://marketplace.visualstudio.com/items?itemName=mrnzdev.neat-theme"
+					title="Neat Theme"
+					status="v0.1.2"
+					description="Sleek black VSCode color theme."
+				/>
 			</ul>
 		</main>
 	);
-}
+};
+export default Projects;
