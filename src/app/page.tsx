51bcfafdd16ad066e6d86ed67a8afb00793e9616
mrnzdev <contact@mrnz.dev>
2024-04-29 08:34:31 -0300

    Refactor page.tsx with import and attribute changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Header from '@/components/header';
-import Projects from '@/components/projects';
+import Header from "@/components/header";
+import Projects from "@/components/projects";
 
-export default function Home() {
+const Home = () => {
 	return (
-		<div className='flex min-dvh-screen flex-col py-8'>
+		<div className="flex min-dvh-screen flex-col py-8">
 			<Header />
 			<Projects />
 		</div>
 	);
-}
+};
+
+export default Home;
