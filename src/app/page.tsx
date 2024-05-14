50f8969018281ee8f12f8de9a715ae3af08ef11b
mrnzdev <contact@mrnz.dev>
2024-05-13 21:33:22 -0300

    chore: Remove unused Projects component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Header from "@/components/header";
-import Projects from "@/components/projects";
 
 const Home = () => {
 	return (
 		<div className="flex min-dvh-screen flex-col py-8">
 			<Header />
-			<Projects />
-		</div>
+\		</div>
 	);
 };
 
