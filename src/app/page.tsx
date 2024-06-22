88c8ad5582de3f0363b021e150bf96d29ec366d8
mrnzdev <contact@mrnz.dev>
2024-06-22 18:01:47 -0300

    Refactor page.tsx to fix closing div tag indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Header from "@/components/header";
+import Header from "@/components/header"
 
 const Home = () => {
 	return (
-		<div className="flex min-dvh-screen flex-col py-8">
+		<div className='flex min-dvh-screen flex-col py-8'>
 			<Header />
 		</div>
-	);
-};
+	)
+}
 
-export default Home;
+export default Home
