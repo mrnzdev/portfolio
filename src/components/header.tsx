8517bf8400d89442f8531be6acd0d02ccb65db6f
mrnzdev <mrnzdev@proton.me>
2024-08-02 11:31:46 -0300

    Formatting components with a different style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,13 +4,13 @@ import GithubIcon from "@/components/github-icon";
 const Header = () => {
 	return (
 		<header className="container">
-			<div className="flex items-center justify-between py-4">
+			<div className="flex justify-between items-center py-4">
 				<a
 					className="flex items-center space-x-2"
 					href="/"
 				>
 					<div className="flex flex-col space-y-1 text-sm leading-none">
-						<h1 className="text-lg font-bold">mrnz</h1>
+						<h1 className="text-lg font-bold">MRTNZ</h1>
 					</div>
 				</a>
 				<div className="flex items-center space-x-2">
@@ -18,9 +18,9 @@ const Header = () => {
 						href="https://github.com/mrnzdev"
 						target="_blank"
 						rel="noreferrer"
-						className="text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background hover:bg-accent hover:text-accent-foreground rounded-md flex h-7 w-7 items-center justify-center p-0"
+						className="ring-offset-background w-7 h-7 flex justify-center items-center p-0 text-sm font-medium rounded-md transition-colors disabled:opacity-50 disabled:pointer-events-none focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 hover:bg-accent hover:text-accent-foreground"
 					>
-						<GithubIcon className="h-4 w-4 fill-current" />
+						<GithubIcon className="w-4 h-4 fill-current" />
 						<span className="sr-only">@mrnzdev</span>
 					</a>
 					<ModeToggle />
--- src/components/project-item.tsx
+++ src/components/project-item.tsx
@@ -9,19 +9,19 @@ const ProjectItem: React.FC<ProjectItemProps> = ({
 	url,
 	title,
 	status,
-	description,
+	description
 }) => (
 	<li>
-		<div className="flex flex-col space-y-1.5">
+		<div className='flex flex-col space-y-1.5'>
 			<div>
-				<span className="font-medium underline underline-offset-4 ">
+				<span className='font-medium underline underline-offset-4'>
 					<a href={url}>{title}</a>
 				</span>
-				<span className="ml-2 rounded-lg px-2 py-0.5 font-mono text-xs no-underline bg-primary text-primary-foreground">
+				<span className='ml-2 rounded-lg px-2 py-0.5 font-mono text-xs no-underline bg-primary text-primary-foreground'>
 					{status}
 				</span>
 			</div>
-			<span className="text-muted-foreground">{description}</span>
+			<span className='text-muted-foreground'>{description}</span>
 		</div>
 	</li>
 );
