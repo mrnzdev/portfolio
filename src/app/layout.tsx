cc7f108543ff273d150cd1969175b4bb368a8433
mrnzdev <mrnzdev@proton.me>
2024-08-02 11:29:28 -0300

    Adding new font to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,26 @@
-import "@/app/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { GeistMono } from "geist/font/mono"
-import type { Metadata } from "next"
+import '@/app/globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Montserrat } from 'next/font/google';
+import type { Metadata } from 'next';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: "MRTNZ",
-	description: "Software Developer"
-}
+	title: 'MRTNZ',
+	description: 'Software Developer'
+};
 
 export default function RootLayout({
 	children
 }: {
-	children: React.ReactNode
+	children: React.ReactNode;
 }) {
 	return (
 		<html
 			lang='en'
-			className={`${GeistMono.className}`}
+			className={`${montserrat.className}`}
 		>
 			<body className='min-h-dvh bg-background text-[0.9rem]'>
 				<ThemeProvider
@@ -33,5 +35,5 @@ export default function RootLayout({
 				</ThemeProvider>
 			</body>
 		</html>
-	)
+	);
 }
