371aad45a0b50e75285db4be195a9d6dee6e4612
mrnzdev <mrnzdev@proton.me>
2024-12-26 11:06:54 +0100

    Adding Not Found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFoundPage() {
+	return (
+		<div className="grid h-dvh grid-cols-1 place-items-center">
+			<main className="text-center text-sm">
+				<h1>404 - Page Not Found</h1>
+				<h2 className="text-zinc-400">
+					The page that you're looking for doesn't exist.
+				</h2>
+			</main>
+		</div>
+	)
+}
